893f19e76bb1084aab2f6016d08fc54f265beda9
sheru007 <jnvh1233sheru@gmail.com>
2024-04-14 21:49:26 +0530

    build added in client

---
+++ client/build/precache-manifest.71fae9c8e4b06b759a158b5f49ecc604.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e9e020550903d67bb51b53e3029117",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09fbfeea11d3b65629ef",
+    "url": "/static/css/main.b7fa03cf.chunk.css"
+  },
+  {
+    "revision": "d728c63edad759487d3f",
+    "url": "/static/js/2.1dd9b9b0.chunk.js"
+  },
+  {
+    "revision": "4704814cfbfc26d598a03749ded69371",
+    "url": "/static/js/2.1dd9b9b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09fbfeea11d3b65629ef",
+    "url": "/static/js/main.92276524.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
