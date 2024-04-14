7fa669c030ceb3cfd4ef5384219f1a35b1f9b514
sheru007 <jnvh1233sheru@gmail.com>
2024-04-14 22:05:55 +0530

    env variable added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const PORT = process.env.PORT || 6000
+const PORT = process.env.PORT || 6000;
 const { MONGOURI } = require('./config/keys')
 
 
